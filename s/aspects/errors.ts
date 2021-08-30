3176b904590631d0e18cf9aee99b0a3e71219c00
Chase Moskal <chasemoskal@gmail.com>
2021-08-30 14:27:12 -0400

    errors: fix names

--- s/aspects/errors.ts
+++ s/aspects/errors.ts
@@ -1,5 +1,8 @@
 
-export class ImportlyError extends Error {}
+export class ImportlyError extends Error {
+	name = this.constructor.name
+}
+
 export class ImportlyParsingError extends ImportlyError {}
 export class ImportlyLookupError extends ImportlyError {}
 export class ImportlyGenerationError extends ImportlyError {}
