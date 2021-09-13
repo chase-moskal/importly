f3cb1ad5a1995040ef3f1547870428565c3b8f73
zenkyuv <przemekgg2002@gmail.com>
2021-09-13 22:37:29 +0200

    generator: fix windows backslashes bug

--- s/aspects/generators/generator-for-node-modules.ts
+++ s/aspects/generators/generator-for-node-modules.ts
@@ -1,33 +1,33 @@
+// import { normalize } from "normalize-path";
+import { ImportMap, Generate } from "../../types.js";
+import { unixNormalize as normalize } from "../../aspects/utilities/unix-normalize.js";
+export const generatorForNodeModules: Generate = ({ root, manifests }) => {
+  const importmap: ImportMap = {
+    imports: {},
+    scopes: {},
+  };
 
-import {normalize} from "path"
-import {ImportMap, Generate} from "../../types.js"
+  function getPath(segments: string[]) {
+    return normalize(root + "/node_modules/" + segments.join("/node_modules/"));
+  }
 
-export const generatorForNodeModules: Generate = ({root, manifests}) => {
-	const importmap: ImportMap = {
-		imports: {},
-		scopes: {},
-	}
+  for (const { entry, parents, label } of manifests) {
+    const isScoped = parents.length > 0;
+    if (isScoped) {
+      const directory = getPath(parents) + "/";
+      const scope = importmap.scopes[directory] ?? {};
+      scope[label + "/"] = getPath([...parents, label]) + "/";
+      if (entry)
+        scope[label] = normalize(getPath([...parents, label]) + "/" + entry);
+      importmap.scopes[directory] = scope;
+    } else {
+      importmap.imports[label + "/"] = getPath([...parents, label]) + "/";
+      if (entry)
+        importmap.imports[label] = normalize(
+          getPath([...parents, label]) + "/" + entry
+        );
+    }
+  }
 
-	function getPath(segments: string[]) {
-		return normalize(root + "/node_modules/" + segments.join("/node_modules/"))
-	}
-
-	for (const {entry, parents, label} of manifests) {
-		const isScoped = parents.length > 0
-		if (isScoped) {
-			const directory = getPath(parents) + "/"
-			const scope = importmap.scopes[directory] ?? {}
-			scope[label + "/"] = getPath([...parents, label]) + "/"
-			if (entry)
-				scope[label] = normalize(getPath([...parents, label]) + "/" + entry)
-			importmap.scopes[directory] = scope
-		}
-		else {
-			importmap.imports[label + "/"] = getPath([...parents, label]) + "/"
-			if (entry)
-				importmap.imports[label] = normalize(getPath([...parents, label]) + "/" + entry)
-		}
-	}
-
-	return importmap
-}
+  return importmap;
+};
